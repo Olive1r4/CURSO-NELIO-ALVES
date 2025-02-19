85e9f13fa59e14e658b1cf44dc42acfaf434a7e2
Fernando Oliveira <fernandooliveira@live.com>
2025-02-18 21:04:58 -0300

    estrutura sequencial - exercicio 2

--- CURSO-NELIO-ALVES/estrutura-sequencial/exercicio2/src/App.java
+++ CURSO-NELIO-ALVES/estrutura-sequencial/exercicio2/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /* 
+            Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos.
+            Fórmula da área: area = π . raio2
+            Considere o valor de π = 3.14159
+        */
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite uma valor para o raio: ");
+        double raio = sc.nextDouble();
+
+        double area = 3.14159*(Math.pow(raio,2));
+        System.out.printf("AREA = %.4f%n", area);
+        sc.close();
     }
 }
